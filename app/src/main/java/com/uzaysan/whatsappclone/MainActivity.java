72b8604c14b89a0a144cb11b9e97799ffb6155ae
uzaysan <alioksuz1126@gmail.com>
2021-05-03 03:27:44 +0300

    Change color and test firebase connection

--- app/src/main/java/com/uzaysan/whatsappclone/MainActivity.java
+++ app/src/main/java/com/uzaysan/whatsappclone/MainActivity.java
@@ -1,14 +1,45 @@
 package com.uzaysan.whatsappclone;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class MainActivity extends AppCompatActivity implements EventListener<QuerySnapshot> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CollectionReference chatRef = FirebaseFirestore.getInstance().collection("Chats");
+        chatRef.addSnapshotListener(this);
+
+    }
+
+
+    @Override
+    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+        if (error != null) {
+            Log.e("Firebase Error: ",error.getMessage());
+            return;
+        }
+        Toast.makeText(this, "Update recieved", Toast.LENGTH_SHORT).show();
+        for (QueryDocumentSnapshot qds : value) {
+            Log.e("Firebase Object: ", qds.toString());
+        }
+
     }
 }
